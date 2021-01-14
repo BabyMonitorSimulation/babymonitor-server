3ad1316684d046ccbac0389b8e89f94d91ebae40
denisousa <denismsb11@gmail.com>
2021-01-14 13:31:01 -0300

    update port

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ from project import app, socketio
 
 
 if __name__ == "__main__":
-    print("BabyMonitor Running")
-    socketio.run(app, port=5000)
+    port = 5000
+    print(f"BabyMonitor Running in {port}")
+    socketio.run(app, port=port)
